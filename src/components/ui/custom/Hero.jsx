e0ca8280e7fd5599acf7d08df33cb52c93961cb7
dworrels <dworrels1@gmail.com>
2024-09-26 19:33:58 -0400

    Add trip data form

--- src/components/ui/custom/Hero.jsx
+++ src/components/ui/custom/Hero.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 import { Button } from "../ui/button"
 
 function Hero() {
@@ -8,8 +9,10 @@ function Hero() {
       >
         <span className='text-[#f56551]'>Discover Your Next Adventure with AI:</span> Personalized itineraries at Your Fingertips</h1>
       <p className='text-xl text-gray-500 text-center'>Your personal trip planner and travel curator, creating custom itineraries tailored to your interest and budget</p>
-    
-      <Button>Get Started, It's Free</Button>
+
+    <Link to={'/create-trip'}>
+      <Button> Get Started, It&apos;s Free </Button>
+    </Link>
     </div>
   )
 }
---
+++ src/constants/options.jsx
@@ -0,0 +1,53 @@
+export const SelectTravelersList = [
+    {
+        id:1,
+        title:'Just Me',
+        desc:'A solo traveler',
+        icon:'✈︎',
+        people:'1 person'
+    },
+    {
+        id:2,
+        title:'Couple',
+        desc:'A couple in love',
+        icon:'👫',
+        people:'2 people'
+    },
+    {
+        id:3,
+        title:'Family',
+        desc:'A family of 3 or more',
+        icon:'👨‍👩‍👧‍👦',
+        people:'3 to 5 people'
+    },
+    {
+        id:4,
+        title:'Group',
+        desc:'A group of friends',
+        icon:'🤜🤛',
+        people:'5 to 10 people'
+    }
+]
+
+export const SelectBudgetOptions = [
+    {
+        id:1,
+        title:'Cheap',
+        desc:'Stay conscious of cost',
+        icon:'💵',
+    },
+    {
+        id:2,
+        title:'Moderate',
+        desc:'Balanced budget',
+        icon:'💰',
+    },
+    {   
+        id:3,
+        title:'Luxury',
+        desc:'High-end experience',
+        icon:'💎',
+    }
+]
+
+export const AI_PROMPT='Generate Travel Plan for Location : {location'
